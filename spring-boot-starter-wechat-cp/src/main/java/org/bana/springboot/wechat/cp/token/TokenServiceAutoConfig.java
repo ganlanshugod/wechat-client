247b5c9824afb03ce4a221d279b7cf33c185b4ec
ganlanshugod <liuwenjiegod@126.com>
2019-11-04 20:35:17 +0800

    升级jar包引用的springboot版本

--- spring-boot-starter-wechat-cp/src/main/java/org/bana/springboot/wechat/cp/token/TokenServiceAutoConfig.java
+++ spring-boot-starter-wechat-cp/src/main/java/org/bana/springboot/wechat/cp/token/TokenServiceAutoConfig.java
@@ -8,6 +8,7 @@
  */
 package org.bana.springboot.wechat.cp.token;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,11 +28,15 @@ import org.bana.wechat.cp.token.JSApiCpService;
 import org.bana.wechat.cp.token.JsApiTicketService;
 import org.bana.wechat.cp.token.SuiteAccessTokenService;
 import org.bana.wechat.cp.token.impl.JSApiCpServiceImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 /**
@@ -42,15 +47,34 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class TokenServiceAutoConfig {
 
+//	@Bean
+//	@ConditionalOnMissingBean(CacheManager.class)
+//	public CacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+//	    RedisCacheManager cacheManager= new RedisCacheManager(redisTemplate);
+//	    cacheManager.setDefaultExpiration(60); //单位秒
+//	    Map<String,Long> expiresMap=new HashMap<String,Long>();
+//	    expiresMap.put("CPToken",3600L);
+//	    cacheManager.setExpires(expiresMap);
+//	    return cacheManager;
+//	}
+	
 	@Bean
+	@ConditionalOnBean(RedisConnectionFactory.class)
 	@ConditionalOnMissingBean(CacheManager.class)
-	public CacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
-	    RedisCacheManager cacheManager= new RedisCacheManager(redisTemplate);
-	    cacheManager.setDefaultExpiration(60); //单位秒
-	    Map<String,Long> expiresMap=new HashMap<String,Long>();
-	    expiresMap.put("CPToken",3600L);
-	    cacheManager.setExpires(expiresMap);
-	    return cacheManager;
+	public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+	    RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        // 默认配置，过期时间指定是30分钟
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        defaultCacheConfig.entryTtl(Duration.ofMinutes(10)); // 单位分钟
+
+        // redisExpire1h cache配置，过期时间指定是1小时，缓存key的前缀指定成prefixaaa_（存到redis的key会自动添加这个前缀）
+        RedisCacheConfiguration userCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().
+                entryTtl(Duration.ofHours(1)).prefixKeysWith("CPToken");
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+        redisCacheConfigurationMap.put("redisExpire1h", userCacheConfiguration);
+
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig, redisCacheConfigurationMap);
+        return cacheManager;
 	}
 
 	/**
--- spring-boot-starter-wechat-mp/src/main/java/org/bana/springboot/wechat/mp/token/MpTokenCacheConfig.java
+++ spring-boot-starter-wechat-mp/src/main/java/org/bana/springboot/wechat/mp/token/MpTokenCacheConfig.java
@@ -1,5 +1,6 @@
 package org.bana.springboot.wechat.mp.token;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,21 +9,37 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 public class MpTokenCacheConfig {
 
 	@Bean
-	@ConditionalOnBean(RedisTemplate.class)
+	@ConditionalOnBean(RedisConnectionFactory.class)
 	@ConditionalOnMissingBean(CacheManager.class)
-	public CacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
-	    RedisCacheManager cacheManager= new RedisCacheManager(redisTemplate);
-	    cacheManager.setDefaultExpiration(60); //单位秒
-	    Map<String,Long> expiresMap=new HashMap<String,Long>();
-	    expiresMap.put("MPToken",3600L);
-	    cacheManager.setExpires(expiresMap);
-	    return cacheManager;
+	public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//	    RedisCacheManager cacheManager= new RedisCacheManager(redisTemplate);
+//	    cacheManager.setDefaultExpiration(60); //单位秒
+//	    Map<String,Long> expiresMap=new HashMap<String,Long>();
+//	    expiresMap.put("MPToken",3600L);
+//	    cacheManager.setExpires(expiresMap);
+	    
+	    
+	    RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        // 默认配置，过期时间指定是30分钟
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        defaultCacheConfig.entryTtl(Duration.ofMinutes(10)); // 单位分钟
+
+        // redisExpire1h cache配置，过期时间指定是1小时，缓存key的前缀指定成prefixaaa_（存到redis的key会自动添加这个前缀）
+        RedisCacheConfiguration userCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().
+                entryTtl(Duration.ofHours(1)).prefixKeysWith("MPToken");
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+        redisCacheConfigurationMap.put("redisExpire1h", userCacheConfiguration);
+
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig, redisCacheConfigurationMap);
+        return cacheManager;
 	}
 }
