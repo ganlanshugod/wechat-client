daa5c4f67a989788715e498b1ec5081218543cb2
zhangzhichao <zzcggsddu@163.com>
2020-05-20 17:50:38 +0800

    退款，签名验证

--- wechat-pay-client/src/main/java/org/bana/wechat/pay/impl/WechatPayServiceImpl.java
+++ wechat-pay-client/src/main/java/org/bana/wechat/pay/impl/WechatPayServiceImpl.java
@@ -37,7 +37,34 @@ public class WechatPayServiceImpl implements WechatPayService{
 	public Map<String, String> unifiedOrder(WxPayParam payParam, Map<String, String> reqData) throws Exception {
 		return wxPayFactory.getWxPay(payParam).unifiedOrder(reqData);
 	}
-	
+	/**
+	* <p>Description: 退款</p> 
+	* @author zhangzhichao   
+	* @date May 20, 2020 5:41:58 PM 
+	* @param payParam
+	* @param reqData
+	* @return
+	* @throws Exception 
+	* @see org.bana.wechat.pay.WechatPayService#refund(org.bana.wechat.pay.app.WxPayParam, java.util.Map) 
+	*/ 
+	@Override
+	public Map<String, String> refund(WxPayParam payParam, Map<String, String> reqData) throws Exception {
+		return wxPayFactory.getWxPay(payParam).refund(reqData);
+	}
+	/**
+	* <p>Description: 验证签名</p> 
+	* @author zhangzhichao   
+	* @date May 20, 2020 5:45:25 PM 
+	* @param payParam
+	* @param reqData
+	* @return
+	* @throws Exception 
+	* @see org.bana.wechat.pay.WechatPayService#isPayResultNotifySignatureValid(org.bana.wechat.pay.app.WxPayParam, java.util.Map) 
+	*/ 
+	@Override
+	public boolean isPayResultNotifySignatureValid(WxPayParam payParam, Map<String, String> reqData) throws Exception {
+		return wxPayFactory.getWxPay(payParam).isPayResultNotifySignatureValid(reqData);
+	}
 	
 	public void setWxPayFactory(WxPayFactory wxPayFactory) {
 		this.wxPayFactory = wxPayFactory;
