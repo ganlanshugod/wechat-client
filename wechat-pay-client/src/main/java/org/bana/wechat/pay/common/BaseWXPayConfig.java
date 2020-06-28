178e64063f696a5f705896bedd31c9be4b4d0e10
ganlanshugod <liuwenjiegod@126.com>
2020-06-28 12:13:35 +0800

    修改默认的wxpay读取方式

--- wechat-pay-client/src/main/java/org/bana/wechat/pay/common/BaseWXPayConfig.java
+++ wechat-pay-client/src/main/java/org/bana/wechat/pay/common/BaseWXPayConfig.java
@@ -1,6 +1,7 @@
 package org.bana.wechat.pay.common;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import com.github.wxpay.sdk.WXPayConfig;
@@ -9,18 +10,28 @@ public abstract class BaseWXPayConfig implements WXPayConfig{
 
     private byte[] certData;
 
-    public BaseWXPayConfig() throws Exception {
+    public BaseWXPayConfig()  {
     	
-        String certPath = "/cert/apiclient_cert.p12";
-        InputStream certStream = BaseWXPayConfig.class.getResourceAsStream(certPath);
-        this.certData = new byte[(int) certStream.available()];
-        certStream.read(this.certData);
-        certStream.close();
     }
 
     public InputStream getCertStream() {
+    	if(certData == null) {
+    		readDefaultCert();
+    	}
         ByteArrayInputStream certBis = new ByteArrayInputStream(this.certData);
         return certBis;
+    }
+    
+    private void readDefaultCert() {
+    	try {
+			String certPath = "/cert/apiclient_cert.p12";
+			InputStream certStream = BaseWXPayConfig.class.getResourceAsStream(certPath);
+			this.certData = new byte[(int) certStream.available()];
+			certStream.read(this.certData);
+			certStream.close();
+		} catch (IOException e) {
+			throw new RuntimeException("no wxPay cert",e);
+		}
     }
 
     public int getHttpConnectTimeoutMs() {
